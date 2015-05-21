8f747159e7257ee60c8c7fb0c9a1e517da7d0254
lo29 <loic.bouilly@aykow.com>
2015-05-21 16:01:33 +0200

    test

--- sys/tcp.c
+++ sys/tcp.c
@@ -33,59 +33,17 @@ void init_tcp(TCP *tcp,DHCP ip){
 }
 
 // Gère l'ouverture d'une session TCP.
-// return : l'état de la connexion.
-uint8_t open_tcp(uint8_t *buf,TCP *tcp){
-	make_tcp_syn(buf,tcp->mac_routeur,tcp->ip_distant,PORT_HTTP_SERVER,PORT_HTTP_ME);
-	do{
-		if(!(packetlen = enc28j60PacketReceive(BUFFER_SIZE, buf))){
-			if(nb_recv > 50){
-				conn->state = ST_ASK_OPEN;
-				return conn->state;
-			}
-		}else{
-			nb_recv++;
-			switch(tcp_recv(buf,packetlen,conn)){
-				case ST_ASK_OPEN:
-
-					return conn->state;
-					break;
-				case ST_CONN :
-					return conn->state;
-					break;
-
-			}
-		}
-	}while(1)
+void open_tcp(uint8_t *buf,TCP tcp){
+	make_tcp_syn(buf,tcp.mac_routeur,tcp.ip_distant,PORT_HTTP_SERVER,PORT_HTTP_ME);
 }
 // Gère la fermeture d'une session TCP.
 int close_tcp(uint8_t *buf){
-	make_tcp_fin(buf,tcp->mac_routeur,tcp->ip_distant,PORT_HTTP_SERVER,PORT_HTTP_ME);
-		if(!(packetlen = enc28j60PacketReceive(BUFFER_SIZE, buf))){
-			if(nb_recv > 50){
-				conn->state = ST_ASK_CLOSE;
-				return conn->state;
-			}
-		}else{
-			nb_recv++;
-			switch(tcp_recv(buf,packetlen,conn)){
-				case ST_ASK_CLOSE:
 
-					return conn->state;
-					break;
-				case ST_DISCONN :
-					return conn->state;
-					break;
-
-			}
-		}
-	}while(1)
-
+return 0;
 }
 // Gère l'envoie d'une trames http.
 void http_send(uint8_t *buf,uint8_t *data){
 
-	fill_tcp_data(buf,0, data);
-	make_tcp_ack_with_data(buf,100);
 
 }
 // Gère la reception d'une trames http. 
@@ -94,7 +52,7 @@ void http_recv(uint8_t *buf){
 
 }
 // Gère la reception d'une trames TCP et réoriente si besoin vers http.
-uint8_t tcp_recv(uint8_t *buf,TCP * conn){
+void tcp_recv(uint8_t *buf,TCP * conn){
 	// On vérifie le type de protocol : 
 	if(buf[IP_PROTO_P] == IP_PROTO_TCP_V){
 		// Les ports :
@@ -106,25 +64,15 @@ uint8_t tcp_recv(uint8_t *buf,TCP * conn){
 					if(conn->state == ST_ASK_OPEN){
 						// On renvoie alors la réponse demandé.	
 						make_tcp_ack_from_any(buf);
-						// et on est alors connecter :
-						conn->state = ST_CONN;
 					}
 					break;
 				case TCP_FLAG_RST_V:
 					// On doit alors recommencer... 
 					conn->state = ST_ASK_OPEN;
 					break;
-				case TCP_FLAGS_FIN_V | TCP_FLAG_ACK_V :
-					make_tcp_ack_from_any(buf);
-					conn->state = ST_DISCONN;
-					break;
-
 			}
-
-			return conn->state;
 		}
 	}
-	return 0;
 }
 
 
@@ -151,13 +99,23 @@ TCP tcp(uint8_t * buf,DHCP ip){
 
 	uint8_t packetlen = 0;
 	uint8_t nb_recv = 0;
-	uint8_t retour;
-	// TO DO: Activé le filtre TCP + ajout du timer.
-	while(open_tcp(buf,&conn) != ST_CONN);
-	// On peut alors envoyer les données reçu au serveur.
-	char * data = "GET / HTTP/1.1\r\nHost: 192.168.0.33\r\nConnection: keep-alive\r\n\r\n";
-	http_send(buf,data);
-	while(close_tcp(buf,&conn) != ST_DISCONN);
+	open_tcp(buf,conn); // TO DO: Activé le filtre TCP + ajout du timer.
+	while(1){
+		if(!(packetlen = enc28j60PacketReceive(BUFFER_SIZE, buf))){
+			if(nb_recv > 50){
+				conn.state = ST_ASK_OPEN;
+				return conn;
+			}
+		}else{
+			nb_recv++;
+			switch(tcp_recv(buf,packetlen,&conn)){
+				case :
+					break;
+                        default:
+                            break;
+			}
+		}
+	}
 }
 
 /******** END DEFINITION DE FONCTIONS **************/
---
+++ sys/tcp.h
@@ -0,0 +1,40 @@
+/* File : tcp.h 
+Author : Aykow
+date : 13 mai 2015
+
+*/
+
+#ifndef TCP_H
+#define TCP_H
+
+#include "defs.h"
+
+#include "net.h"
+#include "ip_arp_udp_tcp.h"
+
+#define ST_CONN 		0x01
+#define ST_DISCONN		0x02
+#define ST_ASK_OPEN		0x03
+#define ST_ASK_CLOSE    0x04
+
+
+// Gère l'initialtisation de la structure.
+void init_tcp(TCP *tcp, DHCP ip);
+// Gère l'ouverture d'une session TCP.
+void open_tcp(uint8_t *buf,TCP * tcp);
+// Gère la fermeture d'une session TCP.
+int close_tcp(uint8_t *buf);
+// Gère l'envoie d'une trames http.
+void http_send(uint8_t *buf,uint8_t *data);
+// Gère la reception d'une trames http. 
+void http_recv(uint8_t *buf);
+// Gère la reception d'une trames TCP et réoriente si besoin vers http.
+void tcp_recv(uint8_t *buf);
+// Gère les paquets TCP.
+TCP tcp(uint8_t* buf,DHCP ip);
+
+
+uint32_t get_seq_num(uint8_t *buf);
+uint32_t get_ack_num(uint8_t *buf);
+
+#endif
